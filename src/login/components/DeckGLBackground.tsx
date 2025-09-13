48e9fcfd0fe0337d96cde87648c7f6aa5de3a9a3
Marc Rufeis <github@craftycram.net>
2025-09-13 11:51:00 +0200

    add more locations

--- src/login/components/DeckGLBackground.tsx
+++ src/login/components/DeckGLBackground.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { LngLatLike } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 interface DeckGLBackgroundProps {
@@ -10,20 +10,28 @@ const DeckGLBackground: React.FC<DeckGLBackgroundProps> = ({ className }) => {
   const mapContainerRef = useRef<HTMLDivElement>(null);
   const mapRef = useRef<mapboxgl.Map | null>(null);
 
+  const locations: { name: string; coordinates: LngLatLike; zoom: number; bearing: number; pitch: number; }[] = [
+    { name: 'Berlin', coordinates: [13.375390, 52.518763], zoom: 16.70, bearing: 55.20, pitch: 72.50 },
+    { name: 'Stuttgart', coordinates: [9.179229, 48.77734], zoom: 17.36, bearing: 152.00, pitch: 75.50 },
+    { name: 'New York City', coordinates: [-73.971274, 40.762698], zoom: 15.60, bearing: 95.94, pitch: 81.38 },
+  ]
+
   useEffect(() => {
     if (!mapContainerRef.current) return;
 
     // TODO: das ist mein token, der muss später getauscht werden
     mapboxgl.accessToken = 'pk.eyJ1IjoiY3JhZnR5Y3JhbSIsImEiOiJjbWZoaWxoZGkwZWtsMmxzZ3M2ajJodnZsIn0.1JbdfnKZZhf1CUByNAElBg';
 
+    const location = locations[Math.floor(Math.random() * locations.length)];
+
     // Initialize Mapbox map with the specified default settings
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/craftycram/cmfhiou9y003c01s589j4au9t', // Keep dark theme
-      center: [13.375390, 52.518763], // Specified coordinates
-      zoom: 16.70, // Specified zoom
-      bearing: 55.20, // Specified bearing
-      pitch: 72.50, // Specified pitch
+      center: location.coordinates, // Specified coordinates
+      zoom: location.zoom, // Specified zoom
+      bearing: location.bearing, // Specified bearing
+      pitch: location.pitch, // Specified pitch
       antialias: true,
       interactive: false, // Disable user interaction
     });
